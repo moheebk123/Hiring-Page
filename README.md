7e02df4ec0e55db2a0d08432a2a073678edfbba9
Moheeb <moheebk522@gmail.com>
2024-10-10 17:52:12 +0530

    complete assignment

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Hiring-Page
+# Hiring Page
+
+This project is a part of a larger system designed to automate the hiring process assigned to me by **The Internet Folks**. It features a multi-step form with a real-time live preview section that updates as users fill out the form.
+
+---
+<img src="./public/project-image.png" />
+
+---
+
+## Technologies Used
+- **Typescript**
+- **Next.js (Framework)**
+- **Chakra UI (Component library)**
+- **Formik (Form handling)**
+
+---
+
+## Task
+### Feature to Add
+1. Values in the draft card should update in real-time based on user input in forms.
+2. Validations are missing from the interview settings form, add validations in the form.
+
+### Bug to Resolve
+- On the job details form when the user submits the form it doesn’t go to the next step even when there are no errors on the screen.
+
+### Additional Feature Added
+- Add responsiveness for all screen sizes.
+
+### Bug Left
+- When clicking on the select box, the dropdown options appear, but the options box is not displayed properly. Additionally, if there are multiple select boxes on the form, clicking on an option from the second or third select box sometimes causes the focus to jump to the next input field or another select box instead of selecting the intended option. This issue occurs when the open dropdown is not the last select box on the form.
+
+- I tried adjusting the positioning and z-index of the select box using CSS, but the issue remains same, so we need to navigate other options through up and down arrow key.
+
+---
+
+## How to Run This Project Locally
+
+Follow these steps to run the application on your system:
+
+1. Clone this repository:
+```bash
+git clone https://github.com/moheebk123/Hiring-Page.git
+```
+2. Open the cloned repository in your preferred code editor.
+3. Install the necessary dependencies:
+
+```bash
+npm install
+```
+
+4. Start the development server:
+
+```bash
+npm run dev
+#or
+yarn dev
+```
+
+5. Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ src/containers/home/InterviewSettingsForm.tsx
@@ -0,0 +1,116 @@
+import { Button, Flex, Box } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import FormSelect from "../../components/formComponents/FormSelect";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { PageNumbers } from "../../interface/home";
+import { IInterViewSettings, State } from "../../interface/forms";
+import {
+  interviewDurationOptions,
+  interviewLanguageOptions,
+  interviewModeOptions,
+} from "./constants";
+import { useData } from "./DataProvider";
+
+const InterviewDetailsForm: React.FC<{
+  handleTab: (n: PageNumbers) => void;
+}> = ({ handleTab }) => {
+  const dataContext = useData();
+
+  if (!dataContext) {
+    throw new Error("useData must be used within a DataProvider");
+  }
+
+  const { state, setState } = dataContext;
+
+  const {
+    errors,
+    touched,
+    handleSubmit,
+    values,
+    setFieldTouched,
+    setFieldValue,
+  } = useFormik<IInterViewSettings>({
+    initialValues: {
+      interviewMode: state?.interviewSettings?.interviewMode || "",
+      interviewDuration: state?.interviewSettings?.interviewDuration || "",
+      interviewLanguage: state?.interviewSettings?.interviewLanguage || "",
+    },
+    validationSchema: Yup.object().shape({
+      interviewMode: Yup.string().required("Interview mode is required"),
+      interviewDuration: Yup.string().required(
+        "Interview duration is required"
+      ),
+      interviewLanguage: Yup.string().required(
+        "Interview language is required"
+      ),
+    }),
+    onSubmit: (values) => {
+      console.log({ values });
+
+      setState((prevState: State) => ({
+        ...prevState,
+        interviewSettings: values,
+      }));
+
+      alert("Form successfully submitted");
+    },
+  });
+
+  useEffect(() => {
+    setState((prevState: State) => ({
+      ...prevState,
+      interviewSettings: values,
+    }));
+  }, [values, setState]);
+
+  return (
+    <Box width="100%" as="form" onSubmit={handleSubmit as any}>
+      <Box width="100%">
+        <FormSelect
+          label="Interview Mode"
+          placeholder="Select interview mode"
+          name="interviewMode"
+          options={interviewModeOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          value={values?.interviewMode}
+          error={errors?.interviewMode}
+          touched={touched?.interviewMode}
+        />
+        <FormSelect
+          label="Interview Duration"
+          placeholder="Select interview duration"
+          name="interviewDuration"
+          options={interviewDurationOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          value={values?.interviewDuration}
+          error={errors?.interviewDuration}
+          touched={touched?.interviewDuration}
+        />
+        <FormSelect
+          label="Interview Language"
+          name="interviewLanguage"
+          placeholder="Select interview language"
+          options={interviewLanguageOptions}
+          onChange={setFieldValue}
+          onBlur={setFieldTouched}
+          error={errors.interviewLanguage}
+          touched={touched.interviewLanguage}
+          value={values.interviewLanguage}
+        />
+        <Flex w="100%" justify="flex-end" mt="4rem" gap="20px">
+          <Button colorScheme="gray" type="button" onClick={() => handleTab(1)}>
+            Previous
+          </Button>
+          <Button colorScheme="red" type="submit">
+            Submit
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default InterviewDetailsForm;
